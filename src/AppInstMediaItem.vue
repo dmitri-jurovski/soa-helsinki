28645b23880ff6c1c5999f0227b948d012550e68
DJ <dmitri.jurovski@gmail.com>
2018-06-23 18:50:05 +0300

    Added InstMediaItem

---
+++ src/AppInstMediaItem.vue
@@ -0,0 +1,14 @@
+<template>
+	<div>
+			 <img v-bind:src="instmedialistitem.images.low_resolution.url">
+			<p>Test</p> 
+	</div>
+</template>
+
+<script>
+console.log("test log");
+	export default {
+      name: 'InstMediaItem',
+      props: ['instmedialistitem'],
+  }
+</script>
